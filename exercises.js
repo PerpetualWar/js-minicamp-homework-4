7c9299e9fb6615408f6ef9f0ec9db8e77281faf2
PerpetualWar <perpetualwarr@gmail.com>
2017-03-17 22:36:55 +0100

    first commit

--- exercises.js
+++ exercises.js
@@ -6,28 +6,47 @@ function multiplyArguments() {
 	//use the arguments keyword to multiply all of the arguments together and return the product
 	//if no arguments are passed in return 0
 	//if one argument is passed in just return it
+	var temp = 1;
+	if(arguments.length !== 0) {
+		for (var i = 0; i < arguments.length; i++) {
+		temp *= arguments[i];
+		}
+	return temp;
+	}else {
+		return 0;
+	}
 }
 
 function invokeCallback(cb) {
 	//invoke cb
+	cb();
 }
 
 function sumArray(numbers, cb) {
 	//sum up all of the integers in the numbers array
 	//pass the result to cb
 	//no return is necessary
+	var sum = 0;
+	for (var i =0; i < numbers.length; i++) {
+		sum += numbers[i];
+	}
+	cb(sum);
 }
 
 function forEach(arr, cb) {
 	//iterate over arr and pass its values to cb one by one
 	//hint: you will be invoking cb multiple times (once for each value in the array)
+	return arr.forEach(cb);
 }
 
 function map(arr, cb) {
 	//create a new array
 	//iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
 	//the new array should be the same length as the array argument
-}
+	//var arr2 = [];
+	return arr.map(cb);
+	}
+
 
 function getUserConstructor() {
 	//create a constructor called User
@@ -36,11 +55,25 @@ function getUserConstructor() {
 	//the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
 	//{{name}} should be the name set on each instance
 	//return the constructor
+	function User(options) {
+		this.username = options.username,
+		this.name = options.name,
+		this.email = options.email,
+		this.password = options.password,
+		User.prototype.sayHi = function() {
+			return 'Hello, my name is ' + this.name;
+		};
+
+	}
+	return User;
 }
 
 function addPrototypeMethod(Constructor) {
 	//add a method to the constructor's prototype
 	//the method should be called 'sayHi' and should return the string 'Hello World!'
+	Constructor.prototype.sayHi = function() {
+		return 'Hello World!';
+	};
 }
 
 function addReverseString() {
@@ -48,6 +81,7 @@ function addReverseString() {
 	//name this method reverse
 	//hint:
 	//you will need to use 'this' inside of reverse
+
 }
 
 function nFactorial(n) {
